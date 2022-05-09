dd480a8fd1d4a66d8765ab4425d02db7c5f70ca5
sebastianvr <sebastianvr92@hotmail.com>
2022-05-08 20:52:56 -0500

    create settings and file main.py #1

---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import ebooklib as eb
+from pathlib import PosixPath
+
+path_books = PosixPath("./datos")
+def check_data_epub():   
+     
+    list_epub_true = []
+    for book in path_books.iterdir():
+        try:
+            if book.suffix == ".epub":
+                return list_epub_true.append(book)
+        except ValueError:
+
+        # print(i.stem, type(i.suffix))
